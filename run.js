6eba464f997c88ecd29c601dc27d0d5a718258fd
Coenie Beyers <coeniebeyers@gmail.com>
2014-06-29 15:54:26 +0200

    Initial hello world commit

---
+++ run.js
@@ -0,0 +1,27 @@
+var autonomy = require('ardrone-autonomy');
+var mission  = autonomy.createMission({ip:'192.168.88.234'});
+
+mission.log('./mission.log')
+       .takeoff()
+       .zero()       // Sets the current state as the reference
+       .altitude(1.5)  // Climb to altitude = 1 meter
+       .forward(1)   
+       .right(1)     
+       .backward(1) 
+       .left(1)
+       .hover(1000)  // Hover in place for 1 second
+       .land();
+
+mission.run(function (err, result) {
+    
+  mission.log('mission.log');
+
+  if (err) {
+    console.trace("Oops, something bad happened: %s", err.message);
+    mission.client().stop();
+    mission.client().land();
+  } else {
+    console.log("Mission success!");
+    process.exit(0);
+  }
+}); 
